c4efab3fbf47bc5a9895550746af636bdcabb307
Ashwani Kumar Singh <ashwanikumarsingh.varanasi@gmail.com>
2023-08-28 19:05:42 +0530

    fixing alert identation

--- lib/common/util/dialog/alert_dialog.dart
+++ lib/common/util/dialog/alert_dialog.dart
@@ -1,142 +1,145 @@
 import 'package:flutter/material.dart';
 
-
 // Just display alert
-simpleAlertDialog(BuildContext context, String title, String subtitle) {  
-  // ok button  
-  Widget okButton = TextButton(  
-    child: const Text("OK"),  
-    onPressed: () {  
-      Navigator.of(context).pop();  
-      
-    },  
-  );  
-  
-  // Create AlertDialog  
-  AlertDialog alert = AlertDialog(  
-    // title: Text(title),  
-    content: SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(title, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
-          const SizedBox(height: 16,),
-          Text(subtitle),
-        ],
-      )
-    ),    
-    actions: [  
-      okButton,  
-    ],  
-  );  
-  
-  // show the dialog  
-  showDialog(  
-    context: context,  
-    builder: (BuildContext context) {  
-      return alert;  
-    },  
-  );  
-}  
+simpleAlertDialog(BuildContext context, String title, String subtitle) {
+  // ok button
+  Widget okButton = TextButton(
+    child: const Text("OK"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
 
+  // Create AlertDialog
+  AlertDialog alert = AlertDialog(
+    // title: Text(title),
+    content: SingleChildScrollView(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Text(subtitle),
+      ],
+    )),
+    actions: [
+      okButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
 
 // Display cancellable alerts
-simpleCancelAlertDialog(BuildContext context, String title, String subtitle, Function okFunction) {  
-  // ok button  
-  Widget okButton = TextButton(  
-    child: const Text("OK"),  
-    onPressed: () {  
+simpleCancelAlertDialog(
+    BuildContext context, String title, String subtitle, Function okFunction) {
+  // ok button
+  Widget okButton = TextButton(
+    child: const Text("OK"),
+    onPressed: () {
       okFunction();
       Navigator.of(context).pop();
-    },  
-  ); 
+    },
+  );
 
   //cancel button
-  Widget cancelButton = TextButton(  
-    child: const Text("Cancel"),  
-    onPressed: () {  
-      Navigator.of(context).pop();  
-    },  
-  );   
-  
-  // Create AlertDialog  
-  AlertDialog alert = AlertDialog(  
-    // title: Text(title),  
-    content: SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(title, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
-          const SizedBox(height: 16,),
-          Text(subtitle),
-        ],
-      )
-    ),  
-    actions: [  
-      okButton, 
-      cancelButton, 
-    ],  
-  );  
-  
-  // show the dialog  
-  showDialog(  
-    context: context,  
-    builder: (BuildContext context) {  
-      return alert;  
-    },  
-  );  
-}  
+  Widget cancelButton = TextButton(
+    child: const Text("Cancel"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
 
-gotoWebpageAlertDialog(BuildContext context, String title, String subtitle, String web) {  
-  // ok button  
-  Widget okButton = TextButton(  
-    child: const Text("OK"),  
-    onPressed: () {  
-      // TODO: 
-      // 1. Go to a custom navigation
-      // 2. Go to a custom page
-      // 2. go to a custom webpage
-      // gotoPage(context, 1, 0);
-      // gotoWebPage(context, web);
-    },  
-  ); 
+  // Create AlertDialog
+  AlertDialog alert = AlertDialog(
+    // title: Text(title),
+    content: SingleChildScrollView(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Text(subtitle),
+      ],
+    )),
+    actions: [
+      okButton,
+      cancelButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
+
+gotoWebpageAlertDialog(
+    BuildContext context, String title, String subtitle, String web) {
+  // ok button
+  Widget okButton = TextButton(
+    child: const Text("OK"),
+    onPressed: () {},
+  );
 
   //cancel button
-  Widget cancelButton = TextButton(  
-    child: const Text("Cancel"),  
-    onPressed: () {  
-      Navigator.of(context).pop();  
-    },  
-  );   
-  
-  // Create AlertDialog  
-  AlertDialog alert = AlertDialog(  
-    // title: Text(title),  
-    content: SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(title, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
-          const SizedBox(height: 16,),
-          Text(subtitle),
-        ],
-      )
-    ),    
-    actions: [  
-      okButton, 
-      cancelButton, 
-    ],  
-  );  
-  
-  // show the dialog  
-  showDialog(  
-    context: context,  
-    builder: (BuildContext context) {  
-      return alert;  
-    },  
-  );  
-}  
+  Widget cancelButton = TextButton(
+    child: const Text("Cancel"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
 
+  // Create AlertDialog
+  AlertDialog alert = AlertDialog(
+    // title: Text(title),
+    content: SingleChildScrollView(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Text(subtitle),
+      ],
+    )),
+    actions: [
+      okButton,
+      cancelButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
